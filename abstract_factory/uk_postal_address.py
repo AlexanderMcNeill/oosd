4a5e6a52eb452de36950f854e7630b1508693732
AlexanderMcNeill <mcnear1@student.op.ac.nz>
2015-03-25 00:03:59 +1300

    Working on uk address class

--- abstract_factory/uk_postal_address.py
+++ abstract_factory/uk_postal_address.py
@@ -2,4 +2,21 @@ __author__ = 'alexmcneill'
 
 
 class UkPostalAddress():
-    pass
+
+    def __init__(self, recipient, org, building_name, address_line, locality, city, post_code):
+        self.recipient = recipient
+        self.org = org
+        self.building_name = building_name
+        self.address_line = address_line
+        self.locality = locality
+        self.city = city
+        self.post_code = post_code
+
+    @property
+    def post_code(self):
+        return self.post_code
+
+    @post_code.setter
+    def post_code(self, post_code):
+        if len(self.post_code) != 7:
+            raise ValueError
